4c34f8f92b7f9515a89102e710dad59256683a48
haryanawisnu <arya.wisnu05@gmail.com>
2017-04-26 13:31:50 +0700

    add functional getall book

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var books = require('./routes/books');
 
 var app = express();
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mongoosecrud');
+mongoose.connect('mongodb://localhost/bookscrud');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/books', books);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ controllers/booksControllers.js
@@ -0,0 +1,92 @@
+var Book = require('../models/book');
+module.exports = {
+  getall: (req, res, next) => {
+    Book.find().exec(function(err, result) {
+      if (result) {
+        res.json(result);
+      } else {
+        res.send(`ERR getall :\n ${err}`);
+      }
+    });
+  },
+  create: (req, res, next) => {
+    let isbn = req.body.isbn;
+    let title = req.body.title;
+    let author = req.body.author;
+    let category = req.body.category;
+    let stock = req.body.stock;
+    MongoClient.connect(url, function(err, db) {
+      console.log("Connected successfully to server bookscrud");
+
+      var collection = db.collection('books');
+      // insert documents
+      collection.insertOne({
+        isbn: isbn,
+        title: title,
+        author: author,
+        category: category,
+        stock: stock
+      }, function(err, result) {
+        if (result) {
+          res.json(result);
+        } else {
+          res.send(`ERR input :\n ${err}`);
+        }
+        db.close();
+      });
+    });
+  },
+  delete: (req, res, next) => {
+    let id = req.params.id;
+    MongoClient.connect(url, function(err, db) {
+      console.log("Connected successfully to server bookscrud");
+
+      var collection = db.collection('books');
+      // delete documents
+      collection.deleteOne({
+        _id: new mongodb.ObjectID(id)
+      }, function(err, result) {
+        if (result) {
+          res.json(result);
+        } else {
+          res.send(`ERR Delete :\n ${err}`);
+        }
+        db.close();
+      });
+    });
+  },
+  update: (req, res, next) => {
+    let id = req.params.id;
+
+    let isbn = req.body.isbn;
+    let title = req.body.title;
+    let author = req.body.author;
+    let category = req.body.category;
+    let stock = req.body.stock;
+
+    MongoClient.connect(url, function(err, db) {
+      console.log("Connected successfully to server bookscrud");
+
+      var collection = db.collection('books');
+      // update documents
+      collection.updateOne({
+        _id: new mongodb.ObjectID(id)
+      }, {
+        $set: {
+          isbn: isbn,
+          title: title,
+          author: author,
+          category: category,
+          stock: stock
+        }
+      }, function(err, result) {
+        if (result) {
+          res.json(result);
+        } else {
+          res.send(`ERR Update :\n ${err}`);
+        }
+        db.close();
+      });
+    });
+  }
+}
