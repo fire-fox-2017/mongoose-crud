c09bcadaf44a8a1b18be84d72e531e0c7b260662
haryanawisnu <arya.wisnu05@gmail.com>
2017-04-26 17:51:21 +0700

    add function update book

--- controllers/booksControllers.js
+++ controllers/booksControllers.js
@@ -31,55 +31,39 @@ module.exports = {
   },
   delete: (req, res, next) => {
     let id = req.params.id;
-    MongoClient.connect(url, function(err, db) {
-      console.log("Connected successfully to server bookscrud");
-
-      var collection = db.collection('books');
-      // delete documents
-      collection.deleteOne({
-        _id: new mongodb.ObjectID(id)
-      }, function(err, result) {
-        if (result) {
-          res.json(result);
-        } else {
-          res.send(`ERR Delete :\n ${err}`);
-        }
-        db.close();
-      });
+    Book.remove({
+      _id: id
+    }, function(err) {
+      if (!err) {
+        res.send(`Success remove with id ${id}`);
+      } else {
+        res.send(`ERR input :\n ${error}`);
+      }
     });
   },
   update: (req, res, next) => {
     let id = req.params.id;
-
     let isbn = req.body.isbn;
     let title = req.body.title;
     let author = req.body.author;
     let category = req.body.category;
     let stock = req.body.stock;
-
-    MongoClient.connect(url, function(err, db) {
-      console.log("Connected successfully to server bookscrud");
-
-      var collection = db.collection('books');
-      // update documents
-      collection.updateOne({
-        _id: new mongodb.ObjectID(id)
-      }, {
-        $set: {
-          isbn: isbn,
-          title: title,
-          author: author,
-          category: category,
-          stock: stock
-        }
-      }, function(err, result) {
-        if (result) {
-          res.json(result);
-        } else {
-          res.send(`ERR Update :\n ${err}`);
-        }
-        db.close();
-      });
+    Book.update({
+      _id: id
+    }, {
+      $set: {
+        isbn: isbn,
+        title: title,
+        author: author,
+        category: category,
+        stock: stock
+      }
+    }, function(err, result) {
+      if (result) {
+        res.json(result);
+      } else {
+        res.send(`ERR Update :\n ${err}`);
+      }
     });
   }
 }
--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,7 @@ var controllers = require('../controllers/booksControllers');
 /* GET home page. */
 router.get('/', controllers.getall);
 router.post('/', controllers.create);
-// router.delete('/:id', controllers.delete);
-// router.put('/:id', controllers.update);
+router.delete('/:id', controllers.delete);
+router.put('/:id', controllers.update);
 
 module.exports = router;
