02b3732f8f5b3f4adea8965f6974920d797f5508
Bhirawa Mbani <bhirmbani@gmail.com>
2017-05-02 20:22:36 +0700

    adding mongoose in app.js

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const bodyParser = require('body-parser');
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.urlencoded({extended: false}));
 
+// mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/library')
+
 // route
 const books = require('./routes/books');
 
