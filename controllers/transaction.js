f026f3ff7e5a883cde0be2ef97f08aa115f89fea
parelhutahaean <parel.hutahaean@gmail.com>
2017-05-03 07:41:34 +0700

    Mongoose CRUD error in transaction insert

---
+++ controllers/transaction.js
@@ -0,0 +1,85 @@
+const mongo = require('mongodb')
+const Transaction = require('../models/transaction')
+let methods = {}
+
+methods.insertOne = (req, res, next) => {
+    Transaction.create(req.body)
+        .then(record => {
+            res.json(record)
+        })
+        .catch(err => {
+            res.json({
+                err,
+                message: 'Error waktu insert Transaction'
+            })
+        })
+} // insertOne
+
+methods.getAll = (req, res) => {
+    Transaction.find({})
+        .populate('booklist') // populate utk mendapatkan informasi semua property dicollection book
+        .exec((err, records) => {
+            if (err) {
+                res.json({
+                    err
+                })
+            } else {
+                console.log(records)
+                res.json(records)
+            }
+        })
+} //getAll
+
+methods.getById = (req, res, next) => {
+    Transaction.findById(req.params.id)
+        .then(record => {
+            res.json(record)
+        })
+        .catch(err => {
+            res.json({
+                err,
+                message: 'Error waktu getById Transaction'
+            })
+        })
+} // getById
+
+methods.updateById = (req, res, next) => {
+    Transaction.findByIdAndUpdate(req.params.id, {
+            $set: {
+                "name": req.body.name || record.name,
+                "memberid": req.body.memberid || record.memberid,
+                "address": req.body.address || record.address,
+                "zipcode": req.body.zipcode || record.zipcode,
+                "phone": req.body.phone || record.phone
+            }
+        }, {
+            new: true // biar data yg ditampilkan data yg terupdate
+        })
+        .exec((err, record) => {
+            if (err) {
+                res.json({
+                    err,
+                    message: 'Error waktu updateById'
+                })
+            } else {
+                res.json(record)
+            }
+        })
+} //updateById
+
+methods.deleteById = (req, res, next) => {
+    Transaction.deleteOne({
+            "_id": new mongo.ObjectID(req.params.id)
+        })
+        .then((record) => {
+            res.json(record)
+        })
+        .catch(err => {
+            res.json({
+                err,
+                message: 'Error waktu deleteById Transaction'
+            })
+        })
+} // deleteById
+
+module.exports = methods
---
+++ models/customer.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let customerSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  memberid: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  zipcode: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  }
+}); // customerSchema
+
+let Customer = mongoose.model('Customer', customerSchema);
+
+module.exports = Customer;
